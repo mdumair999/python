42ed7e7c8ebb1075d98ccebb7052e4c2da86e246
mdumair999 <mdumairahsan@gmail.com>
2024-03-25 16:37:16 +0300

    for-loop (with abs) with readme file

--- for-loop.py
+++ for-loop.py
@@ -1,8 +1,7 @@
-z=[6,4,8]
-target =4 #subtract of 2 nums output should be (2,1)
+z=[6,4,3,8]
+target=4 #subtract of 2 nums
 for i in range(0,len(z)):
-    for j in range(i-1, len(z)):
+    for j in range(abs(-1), len(z)):
         if z[i] - z[j] == target:
             print (i,j)
-
 
