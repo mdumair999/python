9018398d60d23b6422a163d832222385a2ead41d
mdumair999 <mdumairahsan@gmail.com>
2024-03-30 06:46:10 +0300

    input int to check your it is greater or not.

---
+++ input numbers print if true or false.py
@@ -0,0 +1,5 @@
+"""WAP to input 2 int numbers a and b.
+print true if "a" is greater than or equal to "b". if not print false."""
+a = int(input("Enter a number: "))
+b = int(input("Enter a number")) #if a >= b then print True or False.
+print("Your answer is :", a>=b)
